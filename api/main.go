9a7eb7c746857e834b7e2bf6cc461c93d20a1659
hidollara <hidollara@users.noreply.github.com>
2020-08-11 13:28:19 +0900

    mainを整理

--- api/main.go
+++ api/main.go
@@ -1,9 +1,3 @@
 package main
 
-import (
-	_ "github.com/shu22203/internet_ranking/challenge"
-	_ "github.com/shu22203/internet_ranking/ir"
-	_ "github.com/shu22203/internet_ranking/user"
-)
-
 func main() {}
